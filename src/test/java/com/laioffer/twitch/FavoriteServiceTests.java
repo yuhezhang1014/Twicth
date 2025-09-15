f5b6a846cc665fa73028f3d9471aec0ae7abcd1b
CYX <dangeboe@gmail.com>
2025-09-15 16:48:14 -0400

    注释：单元测试注释

--- src/test/java/com/laioffer/twitch/FavoriteServiceTests.java
+++ src/test/java/com/laioffer/twitch/FavoriteServiceTests.java
@@ -26,19 +26,26 @@ public class FavoriteServiceTests {
         favoriteService = new FavoriteService(itemRepository, favoriteRecordRepository);
     }
 
+    /**
+     * 收藏时，item 不存在 → 保存到 item 数据表
+     */
     @Test
     public void whenItemNotExist_setFavoriteItem_shouldSaveItem() {
         UserEntity user = new UserEntity(1L, "user", "foo", "bar", "123456");
         ItemEntity item = new ItemEntity(null, "twitchId", "title", "url", "thumb", "broadcaster", "gameid", ItemType.VIDEO);
         ItemEntity persisted = new ItemEntity(1L, "twitchId", "title", "url", "thumb", "broadcaster", "gameid", ItemType.VIDEO);
         Mockito.when(itemRepository.findByTwitchId("twitchId")).thenReturn(null);
-        Mockito.when(itemRepository.save(item)).thenReturn(persisted);
+        Mockito.when(itemRepository.save(item)).thenReturn(persisted); // 打桩：假设以后有人调用 save(item)，就返回 persisted
 
         favoriteService.setFavoriteItem(user, item);
 
+        // 验证 itemRepository.save(item) 这个调用是否发生过，也就是验证上面的 favoriteService.setFavoriteItem 是否调用了 itemRepository.save(item)
         Mockito.verify(itemRepository).save(item);
     }
 
+    /**
+     * 收藏时，item 存在 → 不保存
+     */
     @Test
     public void whenItemExist_setFavoriteItem_shouldNotSaveItem() {
         UserEntity user = new UserEntity(1L, "user", "foo", "bar", "123456");
@@ -51,6 +58,9 @@ public class FavoriteServiceTests {
         Mockito.verify(itemRepository, Mockito.never()).save(item);
     }
 
+    /**
+     * 取消收藏时，item 不存在 → 不删除记录
+     */
     @Test
     public void whenItemNotExist_unsetFavoriteItem_shouldNotDeleteFavoriteRecord() {
         UserEntity user = new UserEntity(1L, "user", "foo", "bar", "123456");
@@ -61,6 +71,9 @@ public class FavoriteServiceTests {
         Mockito.verifyNoInteractions(favoriteRecordRepository);
     }
 
+    /**
+     * 取消收藏时，item 存在 → 删除记录
+     */
     @Test
     public void whenItemExist_unsetFavoriteItem_shouldDeleteFavoriteRecord() {
         UserEntity user = new UserEntity(1L, "user", "foo", "bar", "123456");
